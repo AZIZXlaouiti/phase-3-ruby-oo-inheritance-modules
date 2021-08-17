c6f98495081589d646a3d7c3e46bef2e64b21025
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-17 19:48:52 -0400

    completed

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+class Kid
+    include Dance  #used as instance method
+    extend MetaDancing #used as class method
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    attr_accessor :name
+    def initialize(name)
+       @name =name    
+    end
+end
